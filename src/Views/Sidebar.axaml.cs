fffe0bd2a1e08470a255efc9f133f98533c00d9f
Duck <61479436+duck-dev@users.noreply.github.com>
2022-10-22 13:05:03 +0200

    Very basic structure in MainWindow.axaml and add Sidebar.axaml
    - Set up a very basic structure in MainWindow.axaml with a purple sidebar and a Grid for the rest of the content.
    - Added a Sidebar.axaml view, which doesn't contain more than a basic Panel now.

---
+++ src/Views/Sidebar.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FileTransfer.Views;
+
+public class Sidebar : UserControl
+{
+    public Sidebar()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
