5998f69d797877bcbed0023dd656e0df8c00f6a7
Duck <61479436+duck-dev@users.noreply.github.com>
2022-10-30 22:50:39 +0100

    Add collection of messages to ReceiveViewModel.cs
    - Added a collection (`ObservableCollection<MessageReceivedEventArgs>`) of received (unread) messages to the `ReceiveViewModel` class. An `ItemsControl`, in which these messages are visually listed, binds to this property.
    - Added a property that retrieves a boolean value, which determines if there are any unread messages. This is used to display or hide content accordingly.

--- src/ViewModels/ReceiveViewModel.cs
+++ src/ViewModels/ReceiveViewModel.cs
@@ -1,7 +1,9 @@
+using System.Collections.ObjectModel;
 using System.Linq;
 using FileTransfer.Models;
 using FileTransfer.Models.NetworkTransmission;
 using FileTransfer.UtilityCollection;
+using ReactiveUI;
 
 namespace FileTransfer.ViewModels;
 
@@ -9,15 +11,19 @@ public sealed class ReceiveViewModel : NetworkViewModelBase
 {
     internal NetworkServer Server { get; }
     
+    private ObservableCollection<MessageReceivedEventArgs> Messages { get; } = new();
+    private bool MessagesAvailable => Messages.Count > 0;
+    
     public ReceiveViewModel() : base(Utilities.UsersList)
     {
         Server = new NetworkServer();
         Server.MessageReceived += OnMessageReceived;
+        Messages.CollectionChanged += (sender, args) => this.RaisePropertyChanged(nameof(MessagesAvailable));
     }
     
     private void OnMessageReceived(object? sender, MessageReceivedEventArgs e)
     {
-        // TODO: Do something
+        Messages.Add(e);
         Utilities.Log($"\nRECEIVED MESSAGE:");
         Utilities.Log($"From: {UsersList?.FirstOrDefault(x => x.UniqueGuid == e.SenderGuid)?.Nickname}");
         Utilities.Log($"Message: {e.TextMessage}\n");
