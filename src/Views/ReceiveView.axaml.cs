f6b3fc78291e10bf443ff81b3125a1d10a4743d2
Duck <61479436+duck-dev@users.noreply.github.com>
2023-01-03 00:12:38 +0100

    Add popup for selecting way of downloading
    - Added a popup for selecting the way the files should be downloaded (as ZIP or to a folder). The flyout/popup items don't execute anything at the moment.

--- src/Views/ReceiveView.axaml.cs
+++ src/Views/ReceiveView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
 
 namespace FileTransfer.Views;
@@ -13,5 +14,20 @@ public class ReceiveView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    internal void ToggleContextMenu(Control button)
+    {
+        MenuFlyout flyout = new MenuFlyout
+        {
+            Items = new []
+            {
+                new MenuItem { Header = "Download ZIP" },
+                new MenuItem { Header = "Download to folder" }
+            },
+            Placement = FlyoutPlacementMode.BottomEdgeAlignedLeft
+        };
+        FlyoutBase.SetAttachedFlyout(button, flyout);
+        FlyoutBase.ShowAttachedFlyout(button);
     }
 }
