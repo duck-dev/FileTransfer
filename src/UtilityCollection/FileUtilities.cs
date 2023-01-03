2efdf9fef9f89556ad39750173ed0d8b74c54793
Duck <61479436+duck-dev@users.noreply.github.com>
2023-01-03 11:51:38 +0100

    Adjust saving files for files with the same name
    - Files that should be downloaded to a folder, where a file with the exact same name already exists, won't overwrite the file or cause a failure of the operation, but they will be saved with a "- Copy" suffix.

--- src/UtilityCollection/FileUtilities.cs
+++ src/UtilityCollection/FileUtilities.cs
@@ -18,6 +18,11 @@ internal static partial class Utilities
     internal static void SaveFilesToFolder(IEnumerable<FileObject> files, string folder)
     {
         foreach (FileObject file in files)
-            file.FileInformation.CopyTo(Path.Combine(folder, file.FileInformation.Name), false);
+        {
+            string path = Path.Combine(folder, file.FileInformation.Name);
+            if (File.Exists(path))
+                path = Path.Combine(folder, $"{Path.GetFileNameWithoutExtension(path)} - Copy{Path.GetExtension(path)}");
+            file.FileInformation.CopyTo(path, false);
+        }
     }
 }
