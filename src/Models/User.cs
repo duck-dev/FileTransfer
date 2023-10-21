09ac886563aa67695d94053bb3ba1083f18f1d7f
Duck <61479436+duck-dev@users.noreply.github.com>
2023-10-21 19:39:57 +0200

    Data changes about user reflect on UI
    - Once data about a user is updated/changed, these changes also reflect on the UI.
    - The initials of the local user are shown on the button, with which the user will be able to open a panel with information about the local user and the possibility to change this information. This panel has not been implemented yet.

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel;
 using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
 using Avalonia.Media;
+using FileTransfer.Interfaces;
 using FileTransfer.UtilityCollection;
 
 namespace FileTransfer.Models;
 
-public class User
+public class User : INotifyPropertyChangedHelper
 {
-    internal User(string id, string nickname, uint colorCode)
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    private string _nickname = string.Empty;
+    private string _id = string.Empty;
+    private string _username = string.Empty;
+    private string _initials = string.Empty;
+    private bool _isOnline = false;
+    
+    [JsonConstructor]
+    public User(string id, string nickname, uint colorCode)
     {
         this.ID = id;
         
@@ -26,13 +39,59 @@ public class User
             this.IP = tempIp;
     }
     
-    public string Nickname { get; set; }
-    public string ID { get; set; }
+    public string Nickname
+    {
+        get => _nickname;
+        set
+        {
+            _nickname = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    public string ID
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            NotifyPropertyChanged();
+        }
+    }
     public uint ColorCode { get; set; } // For JSON
-    
-    internal string Username { get; set; }
-    internal string Initials { get; set; }
+
+    internal string Username
+    {
+        get => _username;
+        set
+        {
+            _username = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    internal string Initials
+    {
+        get => _initials;
+        set
+        {
+            _initials = value;
+            NotifyPropertyChanged();
+        }
+    }
     internal SolidColorBrush ColorBrush { get; set; }
     internal IPAddress? IP { get; set; }
-    internal bool IsOnline { get; set; }
+
+    internal bool IsOnline
+    {
+        get => _isOnline; 
+        set
+        {
+            _isOnline = value;
+            NotifyPropertyChanged();
+        }
+    }
+    
+    public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
