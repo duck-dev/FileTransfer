fc27adb3f5acb3120265dc8b0180107a1793da1c
Duck <61479436+duck-dev@users.noreply.github.com>
2023-11-05 15:00:14 +0100

    Improve contact flyout
    - Improved the contact flyout with the three options (Send Message, Edit Contact, Remove Contact):
        - If the `DataContext` of the passed `Button` is not of type `User`, the method will throw an exception and the execution cannot continue.
        - If the user is not online, no message can be sent to this user (the option "Send Message" is disabled [greyed out]).

--- src/Views/Popups/ContactsListView.axaml.cs
+++ src/Views/Popups/ContactsListView.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Windows.Input;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using FileTransfer.Models;
 using FileTransfer.UtilityCollection;
 using FileTransfer.ViewModels;
 
@@ -30,13 +31,16 @@ public class ContactsListView : UserControl
     private static void ShowFlyout(Control button, ICommand[] commands)
     {
         if (commands.Length < 3)
-            throw new ArgumentException($"The {nameof(commands)} param doesn't contain enough elements (is: {commands.Length}, should be: 3).", nameof(commands));
-        
+            throw new ArgumentException($"The '{nameof(commands)}' param doesn't contain enough elements (is: {commands.Length}, should be: 3).", nameof(commands));
+        if (button.DataContext is not User user)
+            throw new ArgumentException($"The DataContext of the '{nameof(Button)}' param is not of type {nameof(User)}");
+
+        bool isSendingEnabled = user.IsOnline;
         var items = new MenuItem[]
         {
-            new() { Header = "Send message", Command = commands[0], CommandParameter = button.DataContext! },
-            new() { Header = "Edit contact", Command = commands[1], CommandParameter = button.DataContext! },
-            new() { Header = "Remove contact", Command = commands[2], CommandParameter = button.DataContext! }
+            new() { Header = "Send message", Command = commands[0], CommandParameter = button.DataContext, IsEnabled = isSendingEnabled},
+            new() { Header = "Edit contact", Command = commands[1], CommandParameter = button.DataContext },
+            new() { Header = "Remove contact", Command = commands[2], CommandParameter = button.DataContext }
         };
         Utilities.ShowFlyout(button, items, FlyoutPlacementMode.BottomEdgeAlignedLeft);
     }
